9314ed307bb0740baadef307a44a55e4fc37f77a
holywaterBetter <holywater.better@gmail.com>
2023-05-25 16:49:33 +0900

    이름수정: hwayanghouse

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,18 +16,12 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'hwayang-house'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('hwayang-house');
-  });
-
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('.content span')?.textContent).toContain(
-      'hwayang-house app is running!'
+      'hwayanghouse app is running!'
     );
   });
 });
